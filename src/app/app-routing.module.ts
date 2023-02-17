cfc7154f0508bb20b4e64b6adf9cb39212f79c71
Mohamed Ibrahim <crowwnzz3@gmail.com>
2023-02-17 15:13:41 +0530

    Add the User & Common Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminComponent } from './admin/admin.component';
+import { AdminComponent } from './layout/admin/admin.component';
 import { GLoginComponent } from './g-login/g-login.component';
 
 const routes: Routes = [
   {path:'',component:GLoginComponent},
-  {path:'admin', component:AdminComponent ,loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }
+  {path:'admin', component:AdminComponent ,loadChildren: () => import('./layout/admin/admin.module').then(m => m.AdminModule) }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { GLoginComponent } from './g-login/g-login.component';
 import { MaterialModule } from './material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AdminModule } from './admin/admin.module';
+import { AdminModule } from './layout/admin/admin.module';
+import { UserModule } from './layout/user/user.module';
 import { GoogleSigninService } from './google-signin.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';  
 import { MatTableModule } from '@angular/material/table'  
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { CommonSharedModule } from './layout/commonSharedModule';
 
 
 @NgModule({
@@ -26,11 +28,13 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     BrowserAnimationsModule,
     MaterialModule,
     AdminModule,
+    UserModule,
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     MatTableModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    CommonSharedModule
   ],
   providers: [OAuthService, GoogleSigninService, UrlHelperService],
   bootstrap: [AppComponent]
