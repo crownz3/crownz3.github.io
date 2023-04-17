2152641ddf6d7aaa3d7dfd6d367c899187743542
Mohamed Ibrahim <crowwnzz3@gmail.com>
2023-04-17 12:08:59 +0530

    fsdfsdfsdf

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   // path:'http://localhost:3000'
-  // serverBaseUrl:'http://192.168.1.59:3000/'
-  serverBaseUrl:'https://8aa9-136-232-8-106.ngrok-free.app/'
+  serverBaseUrl:'http://localhost:3000/'
+  // serverBaseUrl:'https://8aa9-136-232-8-106.ngrok-free.app/'
 
 };
 
